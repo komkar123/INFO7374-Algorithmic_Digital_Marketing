cfecd2e363b6d274403283d3b0d58aa325176883
komkar123 <omikalange1@gmail.com>
2020-12-14 22:09:52 -0500

    test

---
+++ Project/Streamlit Application/try.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+
+st.markdown("""
+    <iframe width="850" height="450" src="https://datastudio.google.com/embed/reporting/09309c31-918f-4abd-88ed-8ac6fa6c9548/page/g3HtB" frameborder="0" style="border:0" allowfullscreen></iframe>
+    """, unsafe_allow_html=True)
